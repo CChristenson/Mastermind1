eafad782bb877300e1b9cbb60e93f310003ee2bb
Cassie Christenson <cassie@Cassies-MacBook-Pro.local>
2014-12-16 21:02:40 -0500

    Initial Commit

---
+++ Mastermind1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mastermind1
+//
+//  Created by Cassie Christenson on 12/16/14.
+//  Copyright (c) 2014 Cassie and Paul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
